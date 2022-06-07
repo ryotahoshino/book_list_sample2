414159fb345c867c22f0d42055cdd13d6e94a6a2
ryotahoshino <75008198+ryotahoshino@users.noreply.github.com>
2022-06-08 08:59:19 +0900

    image_pickerの追加

--- lib/add_book/add_book_model.dart
+++ lib/add_book/add_book_model.dart
@@ -1,10 +1,15 @@
-import 'package:book_list_sample2/domain/book.dart';
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddBookModel extends ChangeNotifier {
   String? title;
   String? author;
+  File? imageFile;
+
+  final picker = ImagePicker();
 
   Future addBook() async{
     if (title == null || title == ""){
@@ -19,4 +24,13 @@ class AddBookModel extends ChangeNotifier {
       'author': author,
     });
   }
+
+  Future pickImage() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null){
+      imageFile = File(pickedFile.path);
+      notifyListeners();
+    }
+  }
 }
+
